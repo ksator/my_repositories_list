33c03154bdb01a47531ce6c3a77b47901292e0f6
khelil sator <ksator@arista.com>
2024-01-13 20:53:26 +0100

    add action and requirements

--- get_repositories_details.py
+++ get_repositories_details.py
@@ -20,20 +20,23 @@ def get_details_about_a_repo(repo):
     language = str(repo['language'])
     stars = str(repo['stargazers_count'])
     url = repo['html_url']
-    details = 'Name: ' + name + '\n' + 'Description: ' + description + '\n' + 'Topics: ' + topics + '\n' + 'Language: ' + language + '\n' + 'Stars: ' + stars + '\n' + 'URL: ' + url + '\n' + '*'*70
+    details = 'Name: ' + name + '  ' + 'Description: ' + description + '  ' + 'Topics: ' + topics + '  ' + 'Language: ' + language + '  ' + 'Stars: ' + stars + '  ' + 'URL: ' + url + '  ' + '*'*70 + '  '
     return (details)
 
 def main():
     # each github page has a maximum of 30 repositories. 
     # lets start to get all repositories from the first page
+    with open("README.md", 'w') as outfile:
+        outfile.write("here's the list of public repositories under this user that are not comning from a fork")
     page = 1
     user = 'ksator'
     URL = 'https://api.github.com/users/' + user + '/repos?page=' + str(page)
     response = requests.request("GET", URL)
     for repo in response.json():
         if repo['fork'] == False:
-            to_print = get_details_about_a_repo(repo)
-            print(to_print)
+            repo_to_add = get_details_about_a_repo(repo)
+            with open("README.md", 'a') as outfile:
+                outfile.write(repo_to_add)
 
     # lets check if this is the last page. if not, lets get the repository from the next page. 
     while 'rel="last"' in response.headers['link']:
@@ -42,8 +45,9 @@ def main():
         response = requests.request("GET", URL)
         for repo in response.json():
             if repo['fork'] == False:
-                to_print = get_details_about_a_repo(repo)
-                print(to_print)
+                repo_to_add = get_details_about_a_repo(repo)
+                with open("README.md", 'a') as outfile:
+                    outfile.write(repo_to_add)
 
 if __name__ == '__main__':
     main()
