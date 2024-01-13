f2068515774e8e2b897fec1ffb16c8781e385e99
Khelil Sator <khelil.sator.git@gmail.com>
2024-01-13 18:12:18 +0100

    add create get_repositories_details.py

---
+++ get_repositories_details.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+This script prints the list of public repositories from a user.
+For each repository the output includes some details like the repository name, the topics, the language ...
+It doesnt include the repositories comming from a fork.
+"""
+
+import json
+import requests
+
+def get_details_about_a_repo(repo):
+    name = repo['name']
+    description = repo['description']
+    if not description:
+        description = ''
+    topics = str(repo['topics'])
+    if not topics:
+        topics = ''
+    language = str(repo['language'])
+    stars = str(repo['stargazers_count'])
+    url = repo['html_url']
+    details = 'Name: ' + name + '\n' + 'Description: ' + description + '\n' + 'Topics: ' + topics + '\n' + 'Language: ' + language + '\n' + 'Stars: ' + stars + '\n' + 'URL: ' + url + '\n' + '*'*70
+    return (details)
+
+def main():
+    # each github page has a maximum of 30 repositories. 
+    # lets start to get all repositories from the first page
+    page = 1
+    user = 'ksator'
+    URL = 'https://api.github.com/users/' + user + '/repos?page=' + str(page)
+    response = requests.request("GET", URL)
+    for repo in response.json():
+        if repo['fork'] == False:
+            to_print = get_details_about_a_repo(repo)
+            print(to_print)
+
+    # lets check if this is the last page. if not, lets get the repository from the next page. 
+    while 'rel="last"' in response.headers['link']:
+        page = page + 1
+        URL = 'https://api.github.com/users/' + user + '/repos?page=' + str(page)
+        response = requests.request("GET", URL)
+        for repo in response.json():
+            if repo['fork'] == False:
+                to_print = get_details_about_a_repo(repo)
+                print(to_print)
+
+if __name__ == '__main__':
+    main()
