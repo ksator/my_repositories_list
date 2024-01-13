479d8c730c0e9ad35918c2ba6259378175bf8762
Khelil Sator <khelil.sator.git@gmail.com>
2024-01-13 22:19:51 +0100

    Update show_repositories_details.py

--- show_repositories_details.py
+++ show_repositories_details.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 """
-This script prints the list of public repositories from a user.
-For each repository the output includes some details like the repository name, the topics, the language ...
+This script writes in the README.md file the list of public repositories from a GitHub user.
 It doesnt include the repositories comming from a fork.
+For each repository, this script includes some details like the repository name, the topics, the language ...
 """
 
 import json
 import requests
 
-def get_details_about_a_repo(repo):
+def get_details_about_a_repository(repo):
     name = repo['name']
     description = repo['description']
     if not description:
@@ -20,21 +20,21 @@ def get_details_about_a_repo(repo):
     language = str(repo['language'])
     stars = str(repo['stargazers_count'])
     url = repo['html_url']
-    details = 'Name: ' + name + '  \n' + 'Description: ' + description + '  \n' + 'Topics: ' + topics + '  \n' + 'Language: ' + language + '  \n' + 'Stars: ' + stars + '  \n' + 'URL: ' + url + '  \n' + '*'*70 + '  \n'
-    return (details)
+    repository_details = '**Name**: ' + name + '  \n' + '**Description**: ' + description + '  \n' + '**Topics**: ' + topics + '  \n' + '**Language**: ' + language + '  \n' + '**Stars**: ' + stars + '  \n' + '**URL**: ' + url + '  \n' + '*'*70 + '  \n'
+    return (repository_details)
 
 def main():
     # each github page has a maximum of 30 repositories. 
     # lets start to get all repositories from the first page
     with open("README.md", 'w') as outfile:
-        outfile.write("Here's the list of public repositories under this user that are not comning from a fork  \n" + '*'*70 + '  \n')
+        outfile.write("This repository uses a python script and a GitHub action to update periodycally the README.md file with the list of public repositories from my GitHub account. It doesnt include my repositories comming from a fork. For each repository, this script includes some details like the repository name, the topics, the language ...  \n" + '*'*70 + '  \n')
     page = 1
     user = 'ksator'
     URL = 'https://api.github.com/users/' + user + '/repos?page=' + str(page)
     response = requests.request("GET", URL)
     for repo in response.json():
         if repo['fork'] == False:
-            repo_to_add = get_details_about_a_repo(repo)
+            repo_to_add = get_details_about_a_repository(repo)
             with open("README.md", 'a') as outfile:
                 outfile.write(repo_to_add)
 
@@ -45,7 +45,7 @@ def main():
         response = requests.request("GET", URL)
         for repo in response.json():
             if repo['fork'] == False:
-                repo_to_add = get_details_about_a_repo(repo)
+                repo_to_add = get_details_about_a_repository(repo)
                 with open("README.md", 'a') as outfile:
                     outfile.write(repo_to_add)
 
